2a80946af81a7020e147ec213be6eb9b2363b0f4
SergioCheca <55180770+SergioCheca@users.noreply.github.com>
2020-01-08 14:06:10 +0100

    eins

---
+++ KilldaKing_Unity/Assets/Scripts/MovementScript.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovementScript : MonoBehaviour
+{
+    public Transform m_transform;
+    public Vector3 m_destination;
+    public float m_distance;
+    public float m_cadence;
+    public float m_cadenceTimer;
+
+
+    void Start()
+    {
+        m_transform = transform;
+    }
+
+
+    void Update()
+    {
+        
+    }
+}
